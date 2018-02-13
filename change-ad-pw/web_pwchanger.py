059ca03bdb34fffed59c8848d084ce3233634761
Klas Meder Boqvist <github.com@servern.info>
2018-02-13 01:06:25 +0100

    Improved interface by Toby Bowers at Mr Green

--- change-ad-pw/web_pwchanger.py
+++ change-ad-pw/web_pwchanger.py
@@ -16,13 +16,34 @@ class WebUIRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 		self.send_response(200)
 		self.send_header("Content-type", "text/html")
 		self.end_headers()
-		self.wfile.write("""<html><head><title>AD password change</title></head><body>
+		self.wfile.write("""<html><head><title>AD password change</title><link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+<style>*, ::after, ::before {box-sizing: border-box;} body {font-family: 'Lato', sans-serif;} h2 {text-align:center} .box {margin:10px 20px} .box__border {border-radius:10px; padding: 20px;}
+.form__control {display: block; width: 100%; padding: .375rem .75rem; font-size: 1rem; line-height: 1.5; color: #495057;
+background-color: #fff; background-clip: padding-box; border: 1px solid #ced4da; border-radius: .25rem; transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;}
+.form__control:focus {color: #495057; background-color: #fff; border-color: #80bdff; outline: 0; box-shadow: 0 0 0 0.2rem rgba(0,123,255,.25);} .row {display: flex; flex-wrap: wrap}
+.col {flex: 0 1 50%; padding:5px;} .btn { color: #fff; background-color: #007bff; border-color: #007bff; display: inline-block; font-weight: 400; text-align: center; white-space: nowrap; vertical-align: middle; user-select: none; border: 1px solid transparent;
+padding: .375rem .75rem; font-size: 1rem; line-height: 1.5; border-radius: .25rem; transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;}
+.btn:hover {color: #fff; background-color: #0069d9; border-color: #0062cc; text-decoration: none;}
+.form__select {display: inline-block; width: 100%; height: calc(2.25rem + 2px); padding: .375rem 1.75rem .375rem .75rem; line-height: 1.5; color: #495057; vertical-align: middle;
+background-size: 8px 10px; border: 1px solid #ced4da; border-radius: .25rem; appearance: none;}
+.form__select:focus {border-color: #80bdff; outline: 0; box-shadow: inset 0 1px 2px rgba(0,0,0,.075), 0 0 5px rgba(128,189,255,.5);}
+.alert {margin: 1rem 20px 0 20px; color: #721c24; background-color: #f8d7da; border-color: #f5c6cb; position: relative; padding: .75rem 1.25rem; margin-bottom: 1rem; border: 1px solid transparent; border-radius: .25rem;}
+select {margin: 0; font-family: inherit; font-size: inherit; line-height: inherit;}
+</style></head><body>
 <h2>AD password change</h2>""")
 		if msg:
-			self.wfile.write("""<h3 style="color: red">%s</h3>""" % msg)
-		self.wfile.write("""<form method=post><fieldset>
-<p><label for=username>Username</label><input type=text name=username value="%s"></p>
-<p><label for=domain>Select domain</label><select name=domain>""" % username)
+			self.wfile.write("""<div class="alert" role="alert">%s</div>""" % msg)
+		self.wfile.write("""<form class="box" method=post><fieldset class="box__border">
+<div class="row">
+	<div class="col"><input class="form__control" type=text name=username value="%s" placeholder="Username"></div>
+	<div class="col"><input class="form__control" type=password name=old_password autocomplete=off placeholder="Current password"></div>
+</div>
+<div class="row">
+	<div class="col"><input class="form__control" type=password name=new_password1 autocomplete=off placeholder="New password"></div>
+	<div class="col"><input class="form__control" type=password name=new_password2 autocomplete=off placeholder="Repeat new password"></div>
+</div>
+<div class="row">
+	<div class="col"><select class="form__select" name=domain>""" % username)
 		if not prev_domain:
 			self.wfile.write("""<option value="" selected disabled hidden>Select domain</option>""")
 		else:
@@ -32,11 +53,9 @@ class WebUIRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 				self.wfile.write("""<option value="%s">%s</option>""" % (domain, domain))
 			else:
 				self.wfile.write("""<option value="%s" selected>%s</option>""" % (domain, domain))
-		self.wfile.write("""</select></p>
-<p><label for=old_password>Current password</label><input type=password name=old_password autocomplete=off></p>
-<p><label for=new_password1>New password</label><input type=password name=new_password1 autocomplete=off></p>
-<p><label for=new_password2>Repeat new password</label><input type=password name=new_password2 autocomplete=off></p>
-<p><input type=submit value="Change Password"></p>
+		self.wfile.write("""</select></div>
+		<div class="col"><input class="form__control btn" type=submit value="Change Password"></div>
+</div>
 </fieldset>
 </form>
 </body></html>""")
