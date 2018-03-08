f15d973d7762a6e836a47a7d6ec2336d715ae34b
Klas Meder Boqvist <github.com@servern.info>
2018-03-08 15:13:07 +0100

    Remove usage limitation from cert

--- change-ad-pw/SelfSignedCert.ps1
+++ change-ad-pw/SelfSignedCert.ps1
@@ -12,20 +12,12 @@ $key.SecurityDescriptor = "D:PAI(A;;0xd01f01ff;;;SY)(A;;0xd01f01ff;;;BA)(A;;0x80
 $key.MachineContext = 1
 $key.Create()
 
-$serverauthoid = New-Object -COM "X509Enrollment.CObjectId.1"
-$serverauthoid.InitializeFromValue("1.3.6.1.5.5.7.3.1")
-$ekuoids = New-Object -COM "X509Enrollment.CObjectIds.1"
-$ekuoids.Add($serverauthoid)
-$ekuext = New-Object -COM "X509Enrollment.CX509ExtensionEnhancedKeyUsage.1"
-$ekuext.InitializeEncode($ekuoids)
-
 $cert = New-Object -COM "X509Enrollment.CX509CertificateRequestCertificate.1"
 $cert.InitializeFromPrivateKey(2, $key, "")
 $cert.Subject = $name
 $cert.Issuer = $cert.Subject
 $cert.NotBefore = (Get-Date).AddDays(-1)
 $cert.NotAfter = $cert.NotBefore.AddDays($ValidDays)
-$cert.X509Extensions.Add($ekuext)
 $cert.Encode()
 
 $enrollment = New-Object -COM "X509Enrollment.CX509Enrollment.1"
